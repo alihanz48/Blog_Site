6fe484a962a1e459e32c94c85a48231aaeec3361
Alihan Dursun <dursunalihan@gmail.com>
2024-12-06 01:18:46 +0300

    Blog_Site
    Blog yazılarını ekleme, silme ve güncelleme işlemleri ile Admin, Moderatör ve Kullanıcı rollerini içerir. Ölçeklenebilir ve güvenli içerik yönetimi için tasarlandı.

---
+++ blogapp/config.js
@@ -0,0 +1,15 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "123456789",
+        database: "blogdb"
+    },
+    email: {
+        username: "example@gmail.com",
+        password: "izee ekej nzlg vkbm",
+        from: "example@gmail.com",
+    }
+}
+
+module.exports = config;
---
+++ blogapp/middlewares/is-moderator.js
@@ -0,0 +1,13 @@
+const { text } = require("express");
+
+module.exports = (req, res, next) => {
+    if(!req.session.isAuth) {
+        return res.redirect("/account/login?returnUrl=" + req.originalUrl); // => /admin/blogs
+    }
+
+    if(!req.session.roles.includes("admin")&& !req.session.roles.includes("moderator")){
+       req.session.message={text:"Yetkili kullanıcı ile oturum açınız!"};
+       return res.redirect("/account/login?returnUrl="+req.originalUrl);
+    }
+    next();
+}
